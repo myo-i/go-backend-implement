c86dd71d77d6b89205b420c0c83fc530ce73e30a
myo-i <Imiyo0521@gmail.com>
2023-01-17 03:28:50 +0900

    ✨ アカウントと残高のテスト＆ctxが何してるかよくわかってないから要復習

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -12,6 +13,7 @@ func TestTransferTx(t *testing.T) {
 
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
+	fmt.Println(">> before", account1.Balance, account2.Balance)
 
 	// 同時性を注意深く意識しないといけないため、いくつかのgoroutineで処理をする
 	n := 5
@@ -21,8 +23,10 @@ func TestTransferTx(t *testing.T) {
 	results := make(chan TransferTxResult)
 
 	for i := 0; i < n; i++ {
+		txName := fmt.Sprintf("tx %d", i+1)
 		go func() {
-			result, err := store.TransferTx(context.Background(), TransferTxParams{
+			ctx := context.WithValue(context.Background(), txKey, txName)
+			result, err := store.TransferTx(ctx, TransferTxParams{
 				FromAccountID: account1.ID,
 				ToAccountID:   account2.ID,
 				Amount:        amount,
@@ -36,6 +40,7 @@ func TestTransferTx(t *testing.T) {
 	}
 
 	// check results
+	existed := make(map[int]bool)
 	for i := 0; i < n; i++ {
 		err := <-errs
 		require.NoError(t, err)
@@ -80,7 +85,39 @@ func TestTransferTx(t *testing.T) {
 		_, err = store.GetEntry(context.Background(), toEntry.ID)
 		require.NoError(t, err)
 
-		// TODO: chek account's balance
+		// check from accounts
+		fromAccount := result.FromAccount
+		require.NotEmpty(t, fromAccount)
+		require.Equal(t, account1.ID, fromAccount.ID)
 
+		// check to accounts
+		toAccount := result.ToAccount
+		require.NotEmpty(t, toAccount)
+		require.Equal(t, account2.ID, toAccount.ID)
+
+		// chek account's balance(残高)
+		fmt.Println(">> tx", fromAccount.Balance, toAccount.Balance)
+		balance1 := account1.Balance - fromAccount.Balance
+		balance2 := toAccount.Balance - account2.Balance
+		require.Equal(t, balance1, balance2)
+		require.True(t, balance1 > 0)
+		require.True(t, balance1%amount == 0) // amount, 2 * amount, 3 * amount, ..., n * amount（トランザクションの回数分、amountの量が増えるため）
+
+		k := int(balance1 / amount)
+		require.True(t, k >= 1 && k <= n)
+		require.NotContains(t, existed, k)
+		existed[k] = true
 	}
+
+	// check updated balance
+	updateAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updateAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> after", updateAccount1.Balance, updateAccount2.Balance)
+	require.Equal(t, account1.Balance-int64(n)*amount, updateAccount1.Balance)
+	require.Equal(t, int64(n)*amount+account2.Balance, updateAccount2.Balance)
+
 }
