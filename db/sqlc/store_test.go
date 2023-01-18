3748bf5adc7b675d729981766e385611fdddf351
myo-i <Imiyo0521@gmail.com>
2023-01-19 03:36:41 +0900

    ✨ 今まではaccount1からaccount2への送金のみのテストを書いていた。 今回追加したのはaccount1→account2およびaccount2→account1への送金のデッドロックテストを書いた

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -11,6 +11,7 @@ import (
 func TestTransferTx(t *testing.T) {
 	store := NewStore(testDB)
 
+	// このテストではaccount1からaccount2への送金の記録n回を考慮している
 	account1 := createRandomAccount(t)
 	account2 := createRandomAccount(t)
 	fmt.Println(">> before", account1.Balance, account2.Balance)
@@ -119,5 +120,67 @@ func TestTransferTx(t *testing.T) {
 	fmt.Println(">> after", updateAccount1.Balance, updateAccount2.Balance)
 	require.Equal(t, account1.Balance-int64(n)*amount, updateAccount1.Balance)
 	require.Equal(t, int64(n)*amount+account2.Balance, updateAccount2.Balance)
+
+}
+
+func TestTransferTxDeadLock(t *testing.T) {
+	store := NewStore(testDB)
+
+	// このテストではnが偶数回の時account1からaccount2へ
+	// nが奇数回の時account2からaccount1への送金の記録をテストしている
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	fmt.Println(">> before", account1.Balance, account2.Balance)
+
+	// account1からaccount2へ送金、その後account2からaccount1へ送金
+	// 上記で１セットのため偶数回でセットする
+	n := 10
+	amount := int64(10)
+
+	errs := make(chan error)
+
+	for i := 0; i < n; i++ {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if i%2 == 1 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+
+		txName := fmt.Sprintf("tx %d", i+1)
+		go func() {
+			ctx := context.WithValue(context.Background(), txKey, txName)
+			_, err := store.TransferTx(ctx, TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+			})
+
+			// go funcでは条件が満たされていない場合、テスト全体が停止するという保証がないできない
+			// そのため、処理の結果を一旦保持して後で確認する
+			errs <- err
+		}()
+	}
+
+	// check results
+	for i := 0; i < n; i++ {
+		err := <-errs
+		require.NoError(t, err)
+	}
+
+	// check updated balance
+	updateAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+
+	updateAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	fmt.Println(">> after", updateAccount1.Balance, updateAccount2.Balance)
+
+	// 偶数回目でaccount1からaccount2へ、奇数回目でaccount2からaccount1へ送金しているので
+	// 処理の前後で残高に変化がない
+	require.Equal(t, account1.Balance, updateAccount1.Balance)
+	require.Equal(t, account2.Balance, updateAccount2.Balance)
 
 }
