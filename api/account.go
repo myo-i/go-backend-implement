dca457d0267eba959c63bf8a7871427161b69559
myo-i <Imiyo0521@gmail.com>
2023-01-22 03:31:46 +0900

    ✨ アカウント作成& URIからアカウント取得& クエリパラメータからpagenationでアカウントを複数取得

---
+++ api/account.go
@@ -0,0 +1,94 @@
+package api
+
+import (
+	"database/sql"
+	sqlc "go-backend/db/sqlc"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type createAccountRequest struct {
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR"`
+}
+
+func (server *Server) createAccount(ctx *gin.Context) {
+	// アカウントを作成するためのリクエストを送る
+	var req createAccountRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := sqlc.CreateAccountParams{
+		Owner:    req.Owner,
+		Currency: req.Currency,
+		Balance:  0,
+	}
+
+	// リクエストに問題がないにもかかわらずエラーが発生した場合、サーバーエラーとして処理する
+	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccontRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	// リクエストを作成してエラーがないかチェックする
+	var req getAccontRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	// リクエストに問題がないにもかかわらずエラーが発生した場合
+	// idと合致するアカウントが存在しないか、サーバーエラー
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type listAccontRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+// http://localhost:8080/accounts?page_id=1&page_size=7
+// 上記のようなクエリパラメータから複数アカウントを取得
+func (server *Server) listAccount(ctx *gin.Context) {
+	// リクエストを作成してエラーがないかチェックする
+	var req listAccontRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := sqlc.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	accounts, err := server.store.ListAccounts(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
+}
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"go-backend/api"
+	sqlc "go-backend/db/sqlc"
+	"log"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5433/test_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatalln("cannot connect to db", err)
+	}
+
+	store := sqlc.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+
+}
