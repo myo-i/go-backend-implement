bdee9e8316eeca6362881ce2c1b080edfcafee55
myo-i <Imiyo0521@gmail.com>
2023-02-06 00:03:42 +0900

    ✨ srver修正のため、テスト用のserver作成

--- api/main_test.go
+++ api/main_test.go
@@ -1,10 +1,26 @@
 package api
 
 import (
+	db "go-backend/db/sqlc"
+	"go-backend/util"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
+}
 
 // ginをデバッグモードではなく、テストモードで実行することでログを見やすくしている
 func TestMain(m *testing.M) {
